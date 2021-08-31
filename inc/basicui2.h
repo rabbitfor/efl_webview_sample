723ed4bdd867f74258962aa49b8cd21ae5d8b3f3
Jiyun Yang <ji.yang@samsung.com>
2021-08-31 16:30:34 +0900

    Initial
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

---
+++ inc/basicui2.h
@@ -0,0 +1,19 @@
+#ifndef __basicui2_H__
+#define __basicui2_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "basicui2"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.basicui2"
+#endif
+
+#endif /* __basicui2_H__ */
